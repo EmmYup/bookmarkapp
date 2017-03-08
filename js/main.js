1d5a676e77fbfdb6f48fcd6abce4179ecce4e98e
emmanuelyupit <emmanuelyupit08@gmail.com>
2017-03-08 04:37:52 -0500

    restore form

--- js/main.js
+++ js/main.js
@@ -12,6 +12,10 @@ function saveBookmark(e) {
 		url: siteUrl
 	}
 
+	if (!validateForm(siteName, siteUrl)) {
+		return false;
+	}
+
 	/*
 		//Local storage test
 		localStorage.setItem('test', 'Hello world');
@@ -37,6 +41,9 @@ function saveBookmark(e) {
 		//Re-set back to localstorage
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	}
+
+	//Clear form
+	document.getElementById('myForm').reset();
 
 	//Re-fetch bookmarks
 	fetchBookmarks();
@@ -87,4 +94,20 @@ function fetchBookmarks () {
 										'</h3>'
 										'</div>';
 	}
+}
+
+function validateForm (siteName, siteUrl) {
+	if (!siteName || !siteUrl) {
+		alert('Please fill in the form');
+		return false;
+	}
+
+	var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+	var regex = new RegExp(expression);
+
+	if (siteUrl.match(regex)) {
+		alert('Please use a valid URL');
+		return false;
+	}
+	return true;
 }
